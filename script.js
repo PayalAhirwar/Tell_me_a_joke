23c09e0adcad4803a61731ea98f1d713fc309e13
Payal Ahirwar <125037131+PayalAhirwar@users.noreply.github.com>
2023-07-08 17:26:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,325 @@
+let arr = [`Which school supply is the king of the classroom?
+The ruler.`,
+
+  `What runs around the yard (or playground) all day, but never gets tired?
+The fence.`,
+
+  `Why did the teacher have to wear sunglasses?
+Her students were so bright.`,
+
+  `What is a snake's favorite subject?
+Hisssssstory`,
+
+
+  `Why did the crayon cry?
+He was feeling blue`,
+
+  `Where do pencils go on vacation?
+Pennsylvania.`,
+
+  `Why did the dog do so well in school?
+Because he was the teacher’s pet!`,
+
+  `Why did the kid cross the playgrou
+To get to the other slide.`,
+
+  `How do bee parents send their little bees to school?
+They go by school buzz.`,
+
+
+  `Why was the broom late for school?
+It overswept!`,
+
+  `How do you get Pikachu on the bus? 
+You Poke-e-mon (poke him on). `,
+
+  `What do elves do after school?
+GNOME-work`,
+
+  `What is a cat’s favorite color crayon?
+”Purr”ple`,
+
+  `I just can’t remember all the letters of the alphabet…
+I don’t know why!`,
+
+  `What flies around the kindergarten room at night?
+The alpha-BAT.`,
+
+  `What’s the difference between a teacher and a train?
+A teacher says, “Spit out that gum!” and a train says, “Chew! Chew!”`,
+
+
+  `What did the paper say to the pencil? 
+You have a good point!`,
+
+
+  `Why was the music teacher stuck outside his classroom?
+Because his keys were on the piano!`,
+
+
+  `What did the janitor say when he jumped out of the closet?
+Supplies!`,
+
+
+  `Why did the students eat their homework?
+Because the teacher told them that it was a piece of cake.`,
+
+  `Did I tell you the joke about the broken pencil?
+Response: no
+Well, there’s no point.`,
+
+  `What are ten things you can always count on?
+Your fingers!`,
+
+
+  `What do you call a hen who counts her own eggs?
+A mathemachicken.`,
+
+
+  `What did the circle say to the triangle?
+I don’t see your point.`,
+
+  `What was the banker’s favorite player on the football team?
+The quarterback.`,
+
+  `What did 50 do when she got hungry?
+58.`,
+
+
+  `Why is a math book always unhappy?
+Because it always has lots of problems.`,
+
+  `What is a mathematician’s favorite day of the week? 
+Tuesday, because it has a “number” in it. TWOsday.`,
+
+
+  `How do you make seven an even number?
+By removing the ‘`,
+
+  `When is it time to go to the dentist?
+Two-thirty! (Tooth-hurty).`,
+
+
+  `What has hands but can’t clap?
+A clock!`,
+
+
+  `Why is 6 afraid of 7? 
+Because 7-8(ate)-9`,
+
+
+  `There’s a fine line between a numerator and a denominator. 
+Only a fraction of you will get this.`,
+
+  `What did the 0 say to the 8? 
+Nice belt! `,
+
+  `Which tool do you use for math?
+MultiPLIERS!`,
+
+  `What happened when 50 ran a race?
+51`,
+
+  `What did one penny say to the other penny? 
+We make cents!`,
+
+  `What do you call an empty parrot cage?
+A polygon.`,
+
+  `What’s a tornado’s favorite game to play?
+Twister.`,
+
+  `What does the cloud put on before the storm? 
+Thunderpants.`,
+
+  `Why is the moon like a dollar?
+Because it has four quarters`,
+
+  `How does a scientist freshen his breath?
+With experi-mints!`,
+
+  `What kind of flower grows between your nose and your chin?
+Two lips.`,
+
+  `Which planet is the noisiest?
+Saturn, because it has so many rings!
+oke`,
+
+  `What is a vampire’s favorite fruit? 
+A neck-terine`,
+
+  `What is a witch’s favorite subject in school?
+Spelling`,
+
+  `What is a ghost’s favorite pie?
+Boo berry pie`,
+
+  `What do Italian ghosts have for dinner?
+Spook-hetti!`,
+
+  `Where do monsters get an education?
+In ghoul school!`,
+
+  `Why did the turkey join a band?
+So he could use his drumsticks`,
+
+  `If April showers bring May flowers what do May flowers bring? 
+Pilgrims!`,
+
+  `What did one snowman say to the other snowman?
+I smell carrots.`,
+
+  `What do you get if you cross a vampire and a snowman?
+ Frostbite`,
+
+  `What do elves learn in school?
+The elf-abet`,
+
+  ` What often falls in winter, but never gets hurt?
+ Snow`,
+
+  `What type of Mexican food do snowmen like?
+Brrrrrr-itos!`,
+
+  `What is a snowman’s favorite drink?
+Ice Tea`,
+
+  `What treat should you make for the Snowman Holiday Party?
+ Ice Krispy Treats`,
+
+  `What do they sell at McDonald’s at the North Pole?
+Brrrrrrr-gers!`,
+
+  `What does Santa do at football games?
+He gives a little cheer!`,
+
+  `What kind of flower do you never want to get on Valentine’s Day? 
+Cauliflower`,
+
+  `What do you call two birds in love? 
+Tweet-hearts`,
+
+  `What did the calculator say to the other calculator on Valentine’s Day?
+Let me count the ways I love you`,
+
+  `What is a frog’s favorite drink? 
+Croak-a-cola.` ,
+
+  `What do you give a sick lemon?
+Lemon-aid.`,
+
+  `What do you call a sad strawberry?
+ A blueberry`,
+
+  `Why did the banana go to the Doctor?
+Because it wasn’t peeling well!`,
+
+  `What do you call a fake noodle?
+ An im-pasta`,
+
+  `Why did the banana go to the hospital?
+He was peeling really bad.`,
+
+  `What day of the week does the potato look forward to the least?
+Fry-day`,
+
+  `What do you call cheese that doesn’t belong to you?
+Nacho cheese!     ~ Callea J.`,
+
+  `Why did the Oreo go to the dentist?
+Because it lost its filling!`,
+
+  `What do you call a bear with no teeth? 
+A gummy bear!`,
+
+  `Why do eggs hate jokes?
+Because they crack up.`,
+
+  `What are twins’ favorite fruit?
+Pears`,
+
+  `What did Mr. and Mrs. Hamburger name their daughter?
+Patty!`,
+
+  `Why did the cookie go to the doctor? 
+Because he felt “crumby”`,
+
+  `When potatoes have babies, what are they called?
+Tator Tots` ,
+
+  `Where do hamburgers go to dance?
+They go to the meat-ball!`,
+
+  `Why did the elephant cross the road?
+Because it was the chicken’s day off` ,
+
+  `What do a car and an elephant have in common?
+They both have trunks.`,
+
+  `What color of socks do bears wear? 
+They don’t wear socks… they have bear feet (bare feet)` ,
+
+  `How does a penguin build a house?
+Igloos it together`,
+
+  `Two giraffes run a race. 
+They are neck and neck.`,
+
+  `What’s the best day for monkey business?
+The first of Ape-ril`,
+
+  `What do you call bears with no ears?
+b `,
+
+  `What nickname do you keep for a monkey selling potato chips?
+You can call them a chipmunk`,
+
+  `Why can’t a cheetah play hide and seek?
+Because he’s always spotted`,
+
+  `What did the buffalo say when his son went to school?
+Bison!`,
+
+  `What do you call a camel with no humps? 
+Humphrey`,
+
+  `What do monkey cooks wear when they are working in the kitchen?
+They wear the Ape-rons` ,
+
+  `What do you call an alligator in a vest?
+An investigator!`,
+
+  `What do you get when you cross a turtle with a porcupine?
+A slowpoke.`,
+
+  `Which animal cheats in the exams?
+CHEATah`,
+
+  `Which animal is white, black, and red all over the body?
+A little sunburnt penguin!`,
+
+  `What movies do pandas enjoy watching the most?
+They love watching the old movies because the movies are black and white!`,
+
+  `What’s a rabbit’s favorite kind of music?
+Hip_hop`,
+
+  `What did the duck say after she bought Chapstick? 
+Put it on my bill!`,
+
+  `What do you call a horse that lives next door?
+Neigh_bor`,
+
+  `What’s a frog’s favorite game?
+Hopscotch`]                 
+
+const generate_cont = document.getElementById("btn");
+const joke_cont=document.getElementById("joke");
+
+function generateJoke(){
+  const content = arr[Math.floor(Math.random()*arr.length)];
+  joke_cont.textContent = content;
+  
+}
+
+generate_cont.addEventListener("click",generateJoke);
